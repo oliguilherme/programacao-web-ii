5577b4b060e71730c1754f2af94d084583632c4d
oliguilherme <guilhermeleitesouza2018@gmail.com>
2026-09-20 00:24:14 -0300

    chore: adiciona cors

--- todolist/backend/src/app.ts
+++ todolist/backend/src/app.ts
@@ -1,9 +1,13 @@
 import express, { type Express, type Request, type Response } from 'express';
 import taskRouter from './routes/task.routes';
 import { errorMiddleware } from './middlewares/error.middleware';
+import cors from 'cors';
 
 const app: Express = express();
 
+app.use(cors({
+  origin: 'http://localhost:5173'
+}));
 app.use(express.json());
 
 app.get('/', (req: Request, res: Response) => {
